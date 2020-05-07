4f885a001c84c672e06f7b20b21307e4567c061d
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2020-05-06 19:26:47 -0500

    adding aws ses service and post method for booking a room

--- src/dao/get-quote-dao.js
+++ src/dao/get-quote-dao.js
@@ -2,7 +2,28 @@ const pool = require('../config/db-config');
 
 const DatabaseError = require('../errors/database-error');
 
-const getQuoteDao = async (id) => {
+const axios = require('axios');
+let connection = await poolPromise.getConnection();
+axios
+  .post('https://aevpe2f019.execute-api.us-east-2.amazonaws.com/dev/booking/', {
+    checkin: '2020-02-06',
+    checkout: '2020-02-10',
+    email: 'pedro@correo.com',
+    name: 'Pedro',
+    id_room: 'b34f67ea2345',
+  })
+  .then((res) => {
+    connection.query(
+      `INSERT INTO bookings (roomID, checkin, checkout, name, email) VALUES (roomID, checkin, checkout, name, email)`
+    );
+    console.log(`statusCode: ${res.statusCode}`);
+    console.log(res);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+/* const getQuoteDao = async (id) => {
   const poolPromise = pool.promise();
 
   let result;
@@ -19,6 +40,6 @@ const getQuoteDao = async (id) => {
   }
 
   return result[0] || null;
-};
+}; */
 
 module.exports = getQuoteDao;
