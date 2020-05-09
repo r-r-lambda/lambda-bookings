5fd1863c36054e51145b78a2a3584df04bb799cb
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-05-09 00:12:19 -0500

    FIX: Create booking dao fix

--- src/dao/create-booking-dao.js
+++ src/dao/create-booking-dao.js
@@ -7,13 +7,15 @@ const createBookingDao = async (data) => {
 
   let result;
 
+  const { checkin, checkout, email, name, id_room } = data;
+
   try {
     [result] = await poolPromise.query(
       `INSERT INTO booking 
         (checkin, checkout, email, name, id_room) 
       VALUES 
         (?, ?, ?, ?, ?)`,
-      [data.checkin, data.checkout, data.email, data.name, data.id_room]
+      [checkin, checkout, email, name, id_room]
     );
   } catch (error) {
     throw new DatabaseError(error);
@@ -23,9 +25,14 @@ const createBookingDao = async (data) => {
     throw new NotFoundError('No fue posible realizar el booking');
   }
 
-  result[0].id_booking = result.insertId;
-
-  return result[0] || null;
+  return {
+    id_booking: result.insertId,
+    checkin,
+    checkout,
+    email,
+    name,
+    id_room,
+  };
 };
 
 module.exports = createBookingDao;
