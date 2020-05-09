f6c27c7c847aae013e12234bac1b204f7575eaa9
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-05-08 23:29:40 -0500

    REFACTOR: Deleting garbage

--- src/bl/create-email-bl.js
+++
@@ -1,8 +0,0 @@
-
-{
-from: '"Fred Foo 👻" <foo@example.com>', // sender address     
-to: "bar@example.com, baz@example.com", // list of receivers     
-subject: "Hello ✔", // Subject line     
-text: "Hello world?", // plain text body     
-html: "<b>Hello world?</b>" // html body   
-}
