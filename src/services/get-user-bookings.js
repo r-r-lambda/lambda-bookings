9ec4a30f5f18f066c980db91a1902b87eb9f7352
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-06-04 23:22:16 -0500

    FEAT: Get user bookings service implementation

---
+++ src/services/get-user-bookings.js
@@ -0,0 +1,24 @@
+const PropertyRequiedError = require('../errors/property-required-error');
+const getUserBookingsDao = require('../dao/get-user-bookings-dao');
+
+const getUserBookings = async (req, res, next) => {
+  try {
+    const userEmail = req.params.id || null;
+
+    if (!userEmail) {
+      throw new PropertyRequiedError('id');
+    }
+
+    const bookings = await getUserBookingsDao(userEmail);
+
+    if (bookings) {
+      res.status(200).send(bookings);
+    } else {
+      res.status(204).send();
+    }
+  } catch (error) {
+    next(error, req, res, next);
+  }
+};
+
+module.exports = getUserBookings;
